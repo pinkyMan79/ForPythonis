b184f80d960f64327230ff5dd21dcc3fe05197ce
pinkyMan79 <terenindanila5@gmail.com>
2021-11-22 18:55:12 +0300

    I've done

--- HelpTimurka.py
+++ HelpTimurka.py
@@ -33,3 +33,60 @@ for i in range(0, 10):
     c.append(randint(0, 10))
 
 print(buble(c))
+
+# HW #3
+
+# 1
+
+import os
+
+
+def checkForFile(fileName, f):
+    for element in os.scandir(f):
+        if element.is_file():
+            if element.name == fileName:
+                yield f
+        else:
+            yield from checkForFile(fileName, element.path)
+
+
+fName = (str(input()) + '.py') + ''
+print(*checkForFile(fName, os.getcwd()))
+
+# 2 Console
+
+import shutil
+
+path = ''
+
+
+def ls(dir):
+    print(os.listdir(path="" + dir))
+
+
+def cd(way):
+    os.chdir(way)
+
+
+def copy(n1, n2):
+    shutil.copyfile(n1, n2)
+
+
+while (True):
+    inp = input() + ''
+    if inp == "ls":
+        ls(os.getcwd())
+    elif inp == "cd":
+        print("input directory u want to go")
+        i = input()
+        cd(i)
+        print("succes")
+    elif inp == "copy":
+        print("from")
+        i = input()
+        print("to")
+        i1 = input()
+        copy(i, i1)
+    elif inp == "exit":
+        print("bye")
+        break
