5a83721f0dfb092e52e3d28050f0000a7bd17a45
pinkyMan79 <terenindanila5@gmail.com>
2021-11-27 13:02:03 +0300

    With First

--- HelpTimurka.py
+++ HelpTimurka.py
@@ -91,10 +91,9 @@ while (True):
         print("bye")
         break
 
+# 3 Cjntrol work
 
-#3 Cjntrol work
-
-#2
+# 2
 
 n = int(input())
 
@@ -104,7 +103,7 @@ for i in range(n):
     ad = int(input())
     a.append(ad)
 
-for i in range(0, n//2):
+for i in range(0, n // 2):
     t = a[i]
     a[i] = a[n - i - 1]
     a[n - i - 1] = t
@@ -125,7 +124,29 @@ for i in range(n):
 mat2 = matrix
 
 for i in range(n):
-    for j in range(n - 1):
-        mat2[i][j] = (matrix[i][j] + matrix[i][j + 1])
+    for j in range(n):
+        mat2[i][j] = (matrix[i][j - 1] + matrix[i][j])
 
-print(mat2)
+for i in range(n):
+    print(mat2[i])
+
+
+# 1
+
+def sov(n):
+    a = []
+    sum = 0
+    for i in range(n):
+        if n % i == 0:
+            a.append(i)
+    for i in a:
+        sum += i
+    if sum == n:
+        return True
+    else:
+        return False
+
+
+for i in range(1, 5001):
+    if sov(i):
+        print(i)
